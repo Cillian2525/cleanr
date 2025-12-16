fd6c57c0d2e5adbd4563d7b20fc39732f0e8055d
Cillian2525 <hegartycillian04@gmail.com>
2025-12-16 14:47:19 +0000

    Implement detect_outliers using IQR method

--- R/detect_outliers.R
+++ R/detect_outliers.R
@@ -8,5 +8,19 @@
 #' @export
 detect_outliers <- function(x) {
   stopifnot(is.numeric(x))
-  rep(FALSE, length(x))
+
+  # handle case where all values are NA
+  if (all(is.na(x))) {
+    return(rep(FALSE, length(x)))
+  }
+
+  q1 <- stats::quantile(x, 0.25, na.rm = TRUE)
+  q3 <- stats::quantile(x, 0.75, na.rm = TRUE)
+  iqr <- q3 - q1
+
+  lower <- q1 - 1.5 * iqr
+  upper <- q3 + 1.5 * iqr
+
+  x < lower | x > upper
 }
+
