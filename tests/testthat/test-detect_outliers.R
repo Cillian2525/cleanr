12de356707d284d53431cf39c4c336c2c339d40b
Cillian2525 <hegartycillian04@gmail.com>
2025-12-21 16:12:34 +0000

    Fix NA outlier test

--- tests/testthat/test-detect_outliers.R
+++ tests/testthat/test-detect_outliers.R
@@ -1,7 +1,20 @@
-# Basic test to check that detect_outliers returns a logical vector of the correct length
-test_that("detect_outliers returns logical vector", {
+# Basic tests for detect_outliers
+test_that("detect_outliers returns logical vector of correct length", {
   x <- c(1, 2, 3)
   result <- detect_outliers(x)
   expect_type(result, "logical")
-  expect_length(result, 3)
+  expect_length(result, length(x))
+})
+
+test_that("detect_outliers flags extreme values as outliers", {
+  x <- c(1, 2, 3, 100)
+  result <- detect_outliers(x)
+  expect_true(result[4])     # 100 should be an outlier
+  expect_false(result[1])    # 1 should not be an outlier
+})
+
+test_that("detect_outliers handles NA values", {
+  x <- c(1, 2, 3, 4, 5, NA, 1000)
+  result <- detect_outliers(x)
+  expect_true(result[7])     # still detects outlier
 })
